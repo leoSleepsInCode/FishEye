d3ad4059564935a1ca6ef4a1596f7a17be1afb50
Leo-Tena GASSAMA <leotena.gassama@acxess-projects.info>
2023-11-20 10:47:16 +0100

    Photograph Page Part 1

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,3 +2,33 @@
 
 //Mettre le code JavaScript lié à la page photographer.html
 
+function mediaHeaderTemplate(data) {
+    const {name, city, country, tagline} = data;
+
+    function getMediaInformationsDOM() {
+        
+    const picture = `assets/photographers/${portrait}`;
+
+    const section = document.querySelector('photograph-header');
+    const img = document.createElement('img');
+    const figcaption = document.createElement('figcaption');
+    const h1 = document.createElement('h1');
+    const strong = document.createElement('strong');
+    const p = document.createElement('p');
+
+    img.src = picture;
+    h1.textContent = name;
+    strong.textContent = `${city}, ${country}`;
+    p.textContent = tagline;
+
+    section.appendChild(img);
+    section.appendChild(figcaption);
+    figcaption.appendChild(h1);
+    figcaption.appendChild(strong);
+    figcaption.appendChild(p);
+
+    return section;
+    }
+  
+     return {getMediaInformationsDOM }
+}
